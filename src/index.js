020ae21dcb5d956de4f555e2356e3946c48a8908
TheDeGor <degor1983@gmail.com>
2020-05-10 08:44:16 +0600

    express installed, hello world realized

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-const message = 'Hello World';
-console.log(message);
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Got GET request and send you "Hello, world"');
+});
+
+app.post('/', (req, res) => {
+  res.send('Got POST request and send you "Hello, world"');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
