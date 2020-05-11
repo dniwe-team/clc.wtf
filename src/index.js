a3c24a8cf752e6240e6f75ce966f4dfc7b8c6ef4
TheDeGor <degor1983@gmail.com>
2020-05-11 14:21:22 +0600

    Added paths, middleware logger. Start trying to make structure of folders

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ const express = require('express');
 
 const app = express();
 
+
+const myLogger = (req, res, next) => {
+  const msg = (`Дата и время запроса - ${Date()}, метод - ${req.method}, запрос в "${req.path}".`);
+  console.log(msg);
+  next();
+};
+
+app.use(myLogger);
+
 app.get('/', (req, res) => {
   res.send('Got GET request and send you "Hello, world"');
 });
 
 app.post('/', (req, res) => {
   res.send('Got POST request and send you "Hello, world"');
+});
+
+app.get('/about', (req, res) => {
+  res.send('Got GET request and send you "It\'s about"');
+});
+
+app.post('/', (req, res) => {
+  res.send('Got POST request and send you "It\'s about"');
 });
 
 app.listen(3000, () => {
