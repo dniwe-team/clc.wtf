6db6eba30fc8725434eaf3ba9ab9814c991818ec
TheDeGor <degor1983@gmail.com>
2020-05-12 21:44:26 +0600

    Errors fixed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 const express = require('express');
+const middleware = require('./middleware');
 const router = require('./router');
 
 const app = express();
 
-const myLogger = (req, res, next) => {
-  const msg = (`Дата и время запроса - ${Date()}, метод - ${req.method}, запрос в "${req.path}".`);
-  console.log(msg);
-  next();
-};
-
-app.use(myLogger);
-
-app.use('/', router);
+app.use(middleware);
+app.use(router);
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
--- src/middleware/logger/index.js
+++ src/middleware/logger/index.js
@@ -1,11 +1,9 @@
-const express = require('express');
-
-const app = express();
-
-const myLogger = (req, res, next) => {
+const logger = (req, res, next) => {
   const msg = (`Дата и время запроса - ${Date()}, метод - ${req.method}, запрос в "${req.path}".`);
   console.log(msg);
   next();
 };
 
-app.use(myLogger);
+module.exports = {
+  logger: logger,
+};
