0cb3984efb9fad0054726f170a3cfedbe0ba6c6c
TheDeGor <degor1983@gmail.com>
2020-05-13 23:50:39 +0600

    PORT settings added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const config = require('config');
+const dotenv = require('dotenv');
 const middlewares = require('./middleware');
 const router = require('./router');
 
 const app = express();
+dotenv.config();
+const PORT = config.get('port') || process.env.PORT || 3000;
+
+console.log(process.env.PORT);
 
 app.use(middlewares.logger.logger);
 app.use(router);
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
