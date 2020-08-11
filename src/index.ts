b6388d6fc5332932b87791e201fb86a3be1618ca
TheDeGor <degor1983@gmail.com>
2020-08-11 13:42:29 +0600

    TS environment

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,13 @@ const router = require('./router');
 
 const app = express();
 const PORT = process.env.PORT || config.get('port');
+
 app.use(express.static(`${__dirname}/public`));
+
 app.use(express.json());
 app.use(middlewares.logger);
 app.use(router);
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
-// # sourceMappingURL=index.js.map
